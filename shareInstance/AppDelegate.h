8c747e1970f52f145e90ee9ddab9c87fe1f29f75
DengMaojiang <568213372@qq.com>
2016-08-23 22:03:38 +0800

    Initial Commit

---
+++ shareInstance/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  shareInstance
+//
+//  Created by 邓茂江 on 16/8/23.
+//  Copyright © 2016年 taobao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ shareInstance/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  shareInstance
+//
+//  Created by 邓茂江 on 16/8/23.
+//  Copyright © 2016年 taobao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
